3786eb0be9cfe602ce16682cb84f6f4f08061ed1
Saad <bsef21m549@pucit.edu.pk>
2025-03-05 19:35:06 +0500

    added carousel manager component and integrated list carousel , get specific carousel , update carousel and delete carousel api

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,31 @@
+import AdminCarouselApi from "@api/admincarousal.api";
 import { Carousal } from "@components/Carousal";
 import { HomeClothBlock } from "@components/HomeClothBlock";
+import { updateLoader } from "@redux/slices/loaderSlice";
+import { useQuery } from "@tanstack/react-query";
+import { useDispatch } from "react-redux";
 
 const Home = () => {
+
+  const dispatch = useDispatch()
+  const admincarouselapi = new AdminCarouselApi()
+
+  const getAllCarousels = async () =>{
+    dispatch(updateLoader(true));
+    const data = await admincarouselapi.getAllCarousalImages();
+    dispatch(updateLoader(false));
+    return data;
+  }
+
+  const {data} = useQuery({
+    queryKey: ["allcarousel"],
+    queryFn: getAllCarousels
+  })
+
   return (
     <div className="w-full py-4">
       
-    <Carousal />
+    <Carousal data={data?.data}/>
       <div className="mt-8">
         <HomeClothBlock />
         
--- src/routes/constants.ts
+++ src/routes/constants.ts
@@ -21,6 +21,7 @@ export const ROUTE_ADMIN_FOOTWEARPRODUCTS = "/admin-footwearproducts";
 export const ROUTE_ADMIN_ACCESSORIEPRODUCTS = "/admin-accessorieproducts";
 export const ROUTE_ADMIN_ADD_PRODUCTS = "/admin-addproducts";
 export const ROUTE_ADMIN_CUSTOMERS = "/customers";
+export const ROUTE_ADMIN_CAROUSAL = "/carousal";
 export const ROUTE_ADMIN_PROFILE = "/admin-profile";
 export const ROUTE_ADMIN_CHATBOT = "/admin-chatbot";
 export const ROUTE_ADMIN_ORDERS = "/orders";
